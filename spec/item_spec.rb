c4fabaa3db4fee04309644c3440bfcc8028a4ee5
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-03-13 17:16:52 -0700

    add item testing

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
-end
+  it 'has a name and a price' do
+    item = Item.new('FAKE', 100)
+    expect(item.name).to eq('FAKE')
+    expect(item.volume).to eq(100)
+  end
+end 
