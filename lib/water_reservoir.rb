1108a2cec57e015a3579a5bf4779b41834aa2d68
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-03-13 20:31:47 -0700

    update to add floor on drain function and fix variable usage (BUGS documented in code)

--- lib/water_reservoir.rb
+++ lib/water_reservoir.rb
@@ -2,6 +2,7 @@ class WaterReservoir
 
   attr_reader :capacity
   attr_accessor :current_water_volume
+  DRAIN_FLOOR = 0
 
   def initialize(capacity = 10, initial_water_volume = 0)
     @capacity = capacity
@@ -9,15 +10,20 @@ class WaterReservoir
   end
 
   def empty?
-    current_water_volume == 0
+    @current_water_volume == 0 
+    #BUG FIXED not using instance variable
   end
 
   def fill
-    current_water_volume = capacity
+    @current_water_volume = capacity 
+    #BUG FIXED not using instance variable
   end
 
   def drain(volume)
     self.current_water_volume -= volume
+    if (self.current_water_volume < DRAIN_FLOOR)  
+        self.current_water_volume = DRAIN_FLOOR 
+    end
   end
 
 end
