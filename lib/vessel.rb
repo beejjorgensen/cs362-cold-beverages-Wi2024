7acc03f35d4c7ddead821427190c0ee675ca4b2f
Shane Ludwig <Shane.ludwig01@gmail.com>
2024-03-15 14:06:08 -0700

    Vessel.rb added a current water volume, a fill and empty function. The spec was changed to reflect that by checking those empty, function, and default parameters

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,18 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume, :current_water_volume
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @current_water_volume = 0
+  end
+
+  def fill(volume)
+    @current_water_volume = volume
+  end
+  
+  def empty?
+    current_water_volume == 0
   end
 
 end
--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -2,22 +2,27 @@ require_relative '../lib/vessel'
 
 describe 'A vessel for holding liquid' do
 
-  it 'has a name and volume' do
-    vessel = Vessel.new
+  it 'has a name, volume, and current water volume' do
+    vessel = Vessel.new('FAKE', 100)
     expect(vessel.name).to eq('FAKE')
     expect(vessel.volume).to eq(100)
+    expect(vessel.current_water_volume).to eq(0)
   end
 
   it 'is initially empty' do
-    skip
     vessel = Vessel.new('FAKE', 100)
     expect(vessel).to be_empty
   end
 
   it 'is no longer empty when we fill it' do
-    skip
     vessel = Vessel.new('FAKE', 100)
-    vessel.fill
+    vessel.fill(vessel.volume)
     expect(vessel).to_not be_empty
+  end
+
+  it 'should equal its volume when filled' do
+    vessel = Vessel.new('FAKE', 100)
+    vessel.fill(vessel.volume)
+    expect(vessel.current_water_volume).to eq(vessel.volume)
   end
 end
