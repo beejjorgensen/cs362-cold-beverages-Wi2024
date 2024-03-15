b6892f9c99e0279d91758b4b37fad424134e0161
Devon Ceccacci <dceccacci@gmail.com>
2024-03-14 20:28:11 -0700

    added test for initializing item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+  it 'has a name and volume' do
+    item = Item.new("testItem", 10)
+    expect(item.name).to eq("testItem")
+    expect(item.volume).to eq(10)
+  end
 end
