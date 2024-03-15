07933c73994f7b8d05a1fcf67a3711defcd25d0e
Devon Ceccacci <dceccacci@gmail.com>
2024-03-14 20:23:39 -0700

    added new functions and variable
    Added a variable for the current volume of the vessel. Added a function to tell if the vessel is empty, added a function to fill the vessel

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,18 @@
 class Vessel
-  attr_reader :name, :volume
+  attr_reader :name, :volume, :current_volume
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @current_volume = 0
+  end
+
+  def empty?
+    @current_volume == 0
+  end
+
+  def fill
+    @current_volume = @volume
   end
 
 end
