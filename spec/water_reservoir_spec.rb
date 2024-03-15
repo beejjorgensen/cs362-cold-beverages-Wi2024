39c85b8e74e06455f7e9083b1021e2e35c1fb081
Devon Ceccacci <dceccacci@gmail.com>
2024-03-14 21:04:07 -0700

    Tests for water reservoir
    added tests to test the initialization of the reservoir, if it reports empty correctly, if it can be filled, and if it can be drained.

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,33 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it 'with passed paramaters it has capacity and current water volume' do
+    reservoir = WaterReservoir.new(10,0)
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(0)
+  end
+
+  it 'with default paramaters it has capacity and current water volume' do
+    reservoir = WaterReservoir.new
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(0)
+  end
+
+  it 'can report when empty' do
+    reservoir = WaterReservoir.new(10,0)
+    expect(reservoir).to be_empty
+  end
+
+  it 'can fill to capacity' do
+    reservoir = WaterReservoir.new(10,0)
+    reservoir.fill
+    expect(reservoir).not_to be_empty
+  end
+
+  it 'can drain' do
+    reservoir = WaterReservoir.new(10, 10)
+    reservoir.drain(6)
+    expect(reservoir.current_water_volume).to eq(4)
+  end
+
 end
