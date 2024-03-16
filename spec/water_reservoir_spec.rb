9caf1b54628377c2a5f0a9ebf4d06f63f496400a
Liam Colburn <colburnw@oregonstate.edu>
2024-03-16 13:19:15 -0700

    Add tests for reservoir empty?

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -6,4 +6,14 @@ describe 'A water reservoir' do
     expect(reservoir.capacity).to eq(10)
     expect(reservoir.current_water_volume).to eq(8)
   end
+
+  it 'is empty when no volume' do
+    reservoir = WaterReservoir.new(10, 0)
+    expect(reservoir.empty?).to be_truthy
+  end
+
+  it 'is not empty when there is volume' do
+    reservoir = WaterReservoir.new(10, 1)
+    expect(reservoir.empty?).to be_falsey
+  end
 end
