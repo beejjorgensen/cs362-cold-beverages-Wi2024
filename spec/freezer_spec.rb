ad323bebe9da01eab3d550f818e07ceba7aae128
Liam Colburn <colburnw@oregonstate.edu>
2024-03-16 13:36:27 -0700

    Test Freezer temperatures

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -5,4 +5,17 @@ describe 'A freezer' do
     freezer = Freezer.new(50)
     expect(freezer.capacity).to eq(50)
   end
+
+  it 'sets temperature to room temp at level 0' do
+    freezer = Freezer.new
+    freezer.set_level(0)
+    expect(freezer.temperature).to eq(70)
+  end
+
+  it 'sets temperature to 0 at level 7' do
+    freezer = Freezer.new
+    freezer.set_level(7)
+    expect(freezer.temperature).to eq(0)
+  end
+
 end
