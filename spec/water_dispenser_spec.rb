09f739d8f1fa97446532505d5a0602a89948d234
Liam Colburn <colburnw@oregonstate.edu>
2024-03-16 13:26:41 -0700

    Test WaterDispenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+  it 'dispenses water to vessel from reservoir' do
+    reservoir = WaterReservoir.new(10, 10)
+    dispenser = WaterDispenser.new(reservoir)
 
+    vessel = Vessel.new('Cup', 5)
+    dispenser.dispense(vessel)
+  end
 end
