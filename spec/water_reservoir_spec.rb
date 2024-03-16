fae49bba6a5175480836bfce8482aeff6abfa1d9
Liam Colburn <colburnw@oregonstate.edu>
2024-03-16 13:17:37 -0700

    Add test for reservoir variables

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
-
+  it 'has capacity and water volume' do
+    reservoir = WaterReservoir.new(10, 8)
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(8)
+  end
 end
