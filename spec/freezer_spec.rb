fe6f05e92b400ac85e678f2b87a9acf678807afa
Brandon Nelson <nelsonb3@oregonstate.edu>
2024-03-16 22:01:54 -0700

    freezer_spec.rb capacity and contents tests pass

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,15 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+    let(:freezer) { Freezer.new }
+
+    it 'has an initial capacity of 100' do
+        expect(freezer.capacity).to eq(100)
+    end
+
+    it 'has initially empty contents' do
+        expect(freezer.remaining_capacity).to eq(freezer.capacity)
+    end
+
 
 end
