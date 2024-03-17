6c4d6cbddc95f4691b2382a571b308370895c65b
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 17:20:48 -0700

    Add water reservoir attribute specs

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it 'has a capacity and current water volume' do
+    reservoir = WaterReservoir.new
+    expect(reservoir).to respond_to(:capacity)
+    expect(reservoir).to respond_to(:current_water_volume)
+  end
+
 end
