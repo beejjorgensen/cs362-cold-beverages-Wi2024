d1ccd8f27696dfbda2e8bf8e51cc6081871d5e3c
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 17:23:22 -0700

    Add empty? specs

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  
+  let (:reservoir) { WaterReservoir.new }
 
   it 'has a capacity and current water volume' do
-    reservoir = WaterReservoir.new
     expect(reservoir).to respond_to(:capacity)
     expect(reservoir).to respond_to(:current_water_volume)
+  end
+
+  it "can tell when it's empty" do
+    reservoir.current_water_volume = 0
+    expect(reservoir.empty?).to be_truthy
+  end
+
+  it "can tell when it's not empty" do
+    reservoir.current_water_volume = 1
+    expect(reservoir.empty?).to be_falsy
   end
 
 end
