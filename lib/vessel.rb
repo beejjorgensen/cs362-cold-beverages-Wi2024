26c0be9caf9e4ba47bf48d6cf29f966ee80d88eb
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 16:50:10 -0700

    Made unneeded changes before

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,18 +1,10 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume
 
   def initialize(name, volume)
     @name = name
     @volume = volume
     @empty = true
-  end
-
-  def name
-    @name
-  end
-
-  def volume
-    @volume
   end
 
   def empty?
