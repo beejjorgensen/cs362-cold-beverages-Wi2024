a085d4b00ec74df9efa747045d941c5870bff5c0
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 18:27:33 -0700

    Refactor refrigerator specs to use doubles

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -2,9 +2,9 @@ require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
 
-  let (:chiller) { Chiller.new }
-  let (:freezer) { Freezer.new }
-  let (:reservoir) { WaterReservoir.new }
+  let (:chiller) { spy(:chiller) }
+  let (:freezer) { spy(:freezer) }
+  let (:reservoir) { spy(:water_reservoir) }
   let (:dispenser) { WaterDispenser.new(reservoir) }
 
   let (:fridge) { Refrigerator.new(chiller, freezer, dispenser, reservoir) }
@@ -21,26 +21,31 @@ describe 'A refrigerator' do
     item = Item.new("Item", 1)
     fridge.chill(item)
 
-    expect(fridge.chiller.remaining_capacity).to eq(100 - 1)  # Chiller capacity - item volume
+    expect(chiller).to have_received(:add)
   end
 
   it 'can add an item to the freezer' do
     item = Item.new("Item", 1)
     fridge.freeze(item)
 
-    expect(fridge.freezer.remaining_capacity).to eq(100 - 1)  # Freezer capacity - item volume
+    expect(freezer).to have_received(:add)
   end
 
   it "knows it's total capacity" do
+    allow(chiller).to receive(:capacity).and_return(100)
+    allow(freezer).to receive(:capacity).and_return(100)
+
     expect(fridge.total_capacity).to eq(100 + 100)  # Chiller capacity + freezer capacity
   end
 
   it "knows it's remaining capacity" do
     item = Item.new("Item", 2)
     fridge.chill(item)
+    allow(chiller).to receive(:remaining_capacity).and_return(100 - 2)
 
     item = Item.new("Item", 3)
     fridge.freeze(item)
+    allow(freezer).to receive(:remaining_capacity).and_return(100 - 3)
 
     expect(fridge.remaining_capacity).to eq(100 + 100 - 2 - 3)  # Chiller capacity + freezer capacity - both item volumes
   end
@@ -49,32 +54,43 @@ describe 'A refrigerator' do
     fridge.plug_in
 
     expect(fridge.instance_variable_get(:@power)).to eq(:on)
-    expect(fridge.chiller.instance_variable_get(:@power)).to eq(:on)
-    expect(fridge.freezer.instance_variable_get(:@power)).to eq(:on)
+    expect(chiller).to have_received(:turn_on)
+    expect(freezer).to have_received(:turn_on)
   end
 
   it 'can be unplugged' do
     fridge.unplug
 
     expect(fridge.instance_variable_get(:@power)).to eq(:off)
-    expect(fridge.chiller.instance_variable_get(:@power)).to eq(:off)
-    expect(fridge.freezer.instance_variable_get(:@power)).to eq(:off)
+    expect(chiller).to have_received(:turn_off)
+    expect(freezer).to have_received(:turn_off)
   end
 
   it "can set it's chiller's level" do
     fridge.set_chiller_level(5)
 
-    expect(fridge.chiller.temperature).to eq(45)  # level*5 less than room temp (70)
+    expect(chiller).to have_received(:set_level).with(5)
   end
 
   it "can set it's freezer's level" do
     fridge.set_freezer_level(5)
 
-    expect(fridge.freezer.temperature).to eq(20)  # level*10 less than room temp (70)
+    expect(freezer).to have_received(:set_level).with(5)
   end
 
-  it "has a string representation that includes it's power status, remaining storage, temperatures, and remaining water" do
-    expect(fridge.to_s).to eq "Power: off\nStorage: 200 of 200 available\nTemps: Chiller is 70, Freezer is 70\nWater: Reservoir has 0 remaining.\n"
+  it "has a string representation that uses it's power status, remaining storage, temperatures, and remaining water" do
+    allow(fridge).to receive(:remaining_capacity)
+    allow(fridge).to receive(:total_capacity)
+
+    result = fridge.to_s
+
+    expect(result).to include("off")
+
+    expect(fridge).to have_received(:remaining_capacity)
+    expect(fridge).to have_received(:total_capacity)
+    expect(chiller).to have_received(:temperature)
+    expect(freezer).to have_received(:temperature)
+    expect(reservoir).to have_received(:current_water_volume)
   end
 
 end
