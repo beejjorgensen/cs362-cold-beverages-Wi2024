74e2a616c771083c693409ccb77414e3fdeee577
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 17:11:45 -0700

    Add water dispenser attribute specs

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,9 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has a reservoir' do
+    dispenser = WaterDispenser.new(nil)
+    expect(dispenser).to respond_to(:reservoir)
+  end
+
 end
