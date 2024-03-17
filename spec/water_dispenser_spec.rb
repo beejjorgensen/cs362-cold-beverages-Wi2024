bc5b917efb017fb7f74b42e0372e1e2a7bb29a50
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 16:55:11 -0700

    check if water dispensor has a reservoir

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
-
+    it 'has a reservoir' do
+        water_dispenser = WaterDispenser.new('resivoir')
+        expect(water_dispenser.reservoir).to eq('resivoir')
+    end
 end
