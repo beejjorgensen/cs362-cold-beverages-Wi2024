83f7b3565179c2109ecc49757a8337bc03d03358
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 17:42:44 -0700

    Add missing freezer variable specs

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,10 +2,13 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
-  it 'has a capacity and temperature' do
-    freezer = Freezer.new
+  let (:freezer) { Freezer.new }
+
+  it 'has a capacity, temperature, power, and contents' do
     expect(freezer).to respond_to(:capacity)
     expect(freezer).to respond_to(:temperature)
+    expect(freezer.instance_variable_get(:@power)).to eq(:off)
+    expect(freezer.instance_variable_get(:@contents)).to eq([])
   end
 
 end
