27c739998c23c7c7e32aa07137535d313bd1e7f7
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 13:36:47 -0700

    test if despense fills vessel passed

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -14,4 +14,12 @@ describe 'A water dispenser' do
     expect(water_dispenser.dispense(vessel)).to eq(50)
   end
 
+  it 'fills the vessel' do
+    water_reservoir = WaterReservoir.new(200, 150)
+    water_dispenser = WaterDispenser.new(water_reservoir)
+    vessel = Vessel.new('cup', 100)
+    water_dispenser.dispense(vessel)
+    expect(vessel.empty?).to be(false)
+  end
+
 end
