badcadc3243cdd0ed6fcb5ae2e7f7d7e9c5ca83b
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 16:57:45 -0700

    didn't look ahead, make reservoir

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,7 +2,9 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
     it 'has a reservoir' do
-        water_dispenser = WaterDispenser.new('resivoir')
-        expect(water_dispenser.reservoir).to eq('resivoir')
+        reservoir = WaterReservoir.new(100)
+        water_dispenser = WaterDispenser.new(reservoir)
+        expect(water_dispenser.reservoir).to eq(reservoir)
     end
+
 end
