8f07e4f2809b919534b76fe3a14f14982179f9d9
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 11:39:45 -0700

    test written for water dispenser to dispense water

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -7,4 +7,10 @@ describe 'A water dispenser' do
     expect(water_dispenser.reservoir).to be_a(WaterReservoir)
   end
 
+  it 'dispenses water' do
+    water_dispenser = WaterDispenser.new
+    vessel = Vessel.new
+    expect(water_dispenser.dispense(vessel)).to eq('Water dispensed!')
+  end
+
 end
