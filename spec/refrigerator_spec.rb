2c783b3103116cbc8c312c83023910975157e95a
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 12:10:47 -0700

    attribute test written

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -2,4 +2,14 @@ require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
 
+  it 'has a capacity, temp, power, and contents' do
+    fridge = Refrigerator.new(Chiller.new, Freezer.new, WaterReservoir.new)
+    expect(fridge.chiller).to be_a(Chiller)
+    expect(fridge.freezer).to be_a(Freezer)
+    expect(fridge.control_panel).to be_a(ControlPanel)
+    expect(fridge.water_dispenser).to be_a(WaterDispenser)
+    expect(fridge.water_reservoir).to be_a(WaterReservoir)
+    expect(fridge.power).to eq(:off)
+  end
+
 end
