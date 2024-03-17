2fe3a2013c15cc871177d679c4a6869f84222ff1
Xander Bailey <xander.bailey@gmail.com>
2024-03-16 17:39:00 -0700

    Add freezer attribute specs

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
+  it 'has a capacity and temperature' do
+    freezer = Freezer.new
+    expect(freezer).to respond_to(:capacity)
+    expect(freezer).to respond_to(:temperature)
+  end
+
 end
