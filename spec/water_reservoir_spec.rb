e775b26aef40cc0f9bca878a7037700e5c6627af
Tyler <tplaxstar@gmail.com>
2024-03-16 17:42:57 -0700

    water reservoir

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,26 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+    it 'has a capacity and current water volume' do
+        water_reservoir=WaterReservoir.new(10, 10)
+        expect(water_reservoir.capacity).to eq(10)
+        expect(water_reservoir.current_water_volume).to eq(10)
+    end
 
+    it 'is initially empty if unspecified' do
+        water_reservoir = WaterReservoir.new
+        expect(water_reservoir).to be_empty
+    end
+
+    it 'is no longer empty when we fill it' do
+        water_reservoir = WaterReservoir.new
+        water_reservoir.fill
+        expect(water_reservoir).to_not be_empty
+    end
+
+    it 'drains by a given volume' do
+        water_reservoir = WaterReservoir.new(10, 10)
+        water_reservoir.drain(5)
+        expect(water_reservoir.current_water_volume).to eq(5)
+    end
 end
