2412ceea1af71152b789493114c575e81c6491ee
Tyler <tplaxstar@gmail.com>
2024-03-16 17:50:59 -0700

    cleanup

--- lib/water_reservoir.rb
+++ lib/water_reservoir.rb
@@ -9,11 +9,11 @@ class WaterReservoir
   end
 
   def empty?
-    current_water_volume == 0
+    @current_water_volume == 0
   end
 
   def fill
-    current_water_volume = capacity
+    @current_water_volume = capacity
   end
 
   def drain(volume)
