ef8fbb018844405ec5b091ad8d769a7003eee0dc
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 11:07:37 -0700

    reverted changes (went ahead)

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -4,15 +4,6 @@ class Vessel
   def initialize(name = 'FAKE', volume = 100)
     @name = name
     @volume = volume
-    @empty = true
-  end
-
-  def empty?
-    @empty
-  end
-
-  def fill
-    @empty = false
   end
 
 end
