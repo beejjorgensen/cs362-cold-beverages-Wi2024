a4d71721f372938b4324cc08f5d84fcb86b04cbd
Brandon Nelson <nelsonb3@oregonstate.edu>
2024-03-16 22:18:33 -0700

    freezer and chiller spec files with tests complete

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
+    let(:refrigerator) { Refrigerator.new }
+
+    it 'exists as a working fridge' do
+        
+    end
 
 end
