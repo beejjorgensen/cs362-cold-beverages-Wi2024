289eb0daa667f8c1dcde969c52623cf59973fd7b
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 11:05:33 -0700

    added empty flag and method, attr_reader for name, default params and fill method

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,18 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume
 
-  def initialize(name, volume)
+  def initialize(name = 'FAKE', volume = 100)
     @name = name
     @volume = volume
+    @empty = true
+  end
+
+  def empty?
+    @empty
+  end
+
+  def fill
+    @empty = false
   end
 
 end
