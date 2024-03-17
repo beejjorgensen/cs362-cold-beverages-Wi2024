84bf5902e19519f38229b92debb8537b46a7df49
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 12:14:47 -0700

    changed control panel to undefined object

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -3,10 +3,10 @@ require_relative '../lib/refrigerator'
 describe 'A refrigerator' do
 
   it 'has a capacity, temp, power, and contents' do
-    fridge = Refrigerator.new(Chiller.new, Freezer.new, ControlPanel.new, WaterDispenser.new, WaterReservoir.new)
+    fridge = Refrigerator.new(Chiller.new, Freezer.new, :control_panel, WaterDispenser.new, WaterReservoir.new)
     expect(fridge.chiller).to be_a(Chiller)
     expect(fridge.freezer).to be_a(Freezer)
-    expect(fridge.control_panel).to be_a(ControlPanel)
+    expect(fridge.control_panel).to eq(:control_panel)
     expect(fridge.water_dispenser).to be_a(WaterDispenser)
     expect(fridge.water_reservoir).to be_a(WaterReservoir)
     expect(fridge.power).to eq(:off)
