0b0f5eb92614c1f6c4f7e3c5bc82f8f1f5c474d0
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 12:12:01 -0700

    added missing args

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/refrigerator'
 describe 'A refrigerator' do
 
   it 'has a capacity, temp, power, and contents' do
-    fridge = Refrigerator.new(Chiller.new, Freezer.new, WaterReservoir.new)
+    fridge = Refrigerator.new(Chiller.new, Freezer.new, ControlPanel.new, WaterDispenser.new, WaterReservoir.new)
     expect(fridge.chiller).to be_a(Chiller)
     expect(fridge.freezer).to be_a(Freezer)
     expect(fridge.control_panel).to be_a(ControlPanel)
