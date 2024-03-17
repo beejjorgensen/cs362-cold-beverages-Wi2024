6831169f3d5a4ff860c9ea43ab4f46c5969b945f
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-17 11:33:44 -0700

    water dispender attribute test written

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,9 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has a water reservoir' do
+    water_dispenser = WaterDispenser.new
+    expect(water_dispenser.reservoir).to be_a(WaterReservoir)
+  end
+
 end
