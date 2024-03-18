46b60e69716118f240f88d0ce1d66967206d0569
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 17:45:16 -0700

    test fridge capacity in chiller & freezer

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -34,4 +34,13 @@ describe 'A refrigerator' do
         refrigerator.freeze(item)
         expect(refrigerator.freezer.contents).to eq([item])
     end
+
+    it 'can return the total capacity of the chiller and freezer' do
+        chiller = Chiller.new(100)
+        freezer = Freezer.new(100)
+        water_reservoir = WaterReservoir.new()
+        water_dispenser = WaterDispenser.new(water_reservoir)
+        refrigerator = Refrigerator.new(chiller, freezer, water_dispenser, water_reservoir)
+        expect(refrigerator.total_capacity).to eq(200)
+    end
 end
