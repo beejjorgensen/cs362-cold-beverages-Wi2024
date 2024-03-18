ee8412f33dec4639b6d5edb4b430eb7ec391f21d
kameron beamer <beamerkameron@gmail.com>
2024-03-18 04:07:14 +0000

    Got item attributes under test

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/chiller'
 
 describe 'A chiller' do
-
+    it "has capacity and temprature" do
+        chiller = Chiller.new
+        expect(chiller.capacity).to eq(100)
+        expect(chiller.temperature).to eq(70)
+    end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+    it "has name and volume" do
+        item = Item.new
+        expect(item.name).to eq("FAKE")
+        expect(item.volume).to eq(100)
+    end
 end
