9d32ecaa72c5ff02b75aaaa195f178475286e929
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 18:06:41 -0700

    move out object repetative initialization

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,15 +1,14 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+    let(:reservoir) { WaterReservoir.new(100, 100) }
+    let(:water_dispenser) { WaterDispenser.new(reservoir) }
+
     it 'has a reservoir' do
-        reservoir = WaterReservoir.new(100)
-        water_dispenser = WaterDispenser.new(reservoir)
         expect(water_dispenser.reservoir).to eq(reservoir)
     end
 
     it 'dispenses water from the reservoir' do
-        reservoir = WaterReservoir.new(100, 100)
-        water_dispenser = WaterDispenser.new(reservoir)
         vessel = Vessel.new('FAKE', 100)
         water_dispenser.dispense(vessel)
         expect(reservoir.current_water_volume).to eq(0)
