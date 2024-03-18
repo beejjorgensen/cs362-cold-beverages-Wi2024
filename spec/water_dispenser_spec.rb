04f8668f8236f5a6cf373cebc0b691e5f2e7ee91
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 17:04:11 -0700

    Test dispense method in water_dispencer.rb

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -7,4 +7,11 @@ describe 'A water dispenser' do
         expect(water_dispenser.reservoir).to eq(reservoir)
     end
 
+    it 'dispenses water from the reservoir' do
+        reservoir = WaterReservoir.new(100, 100)
+        water_dispenser = WaterDispenser.new(reservoir)
+        vessel = Vessel.new('FAKE', 100)
+        water_dispenser.dispense(vessel)
+        expect(reservoir.current_water_volume).to eq(0)
+    end
 end
