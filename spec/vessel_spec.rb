84ad73ccda060a078d9c08929b0c3c45e48b3589
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 18:07:30 -0700

    move out repetative object initialization

--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -1,20 +1,18 @@
 require_relative '../lib/vessel'
 
 describe 'A vessel for holding liquid' do
+  let(:vessel) { Vessel.new('FAKE', 100) }
 
   it 'has a name and volume' do
-    vessel = Vessel.new('FAKE', 100)
     expect(vessel.name).to eq('FAKE')
     expect(vessel.volume).to eq(100)
   end
 
   it 'is initially empty' do
-    vessel = Vessel.new('FAKE', 100)
     expect(vessel).to be_empty
   end
 
   it 'is no longer empty when we fill it' do
-    vessel = Vessel.new('FAKE', 100)
     vessel.fill
     expect(vessel).to_not be_empty
   end
