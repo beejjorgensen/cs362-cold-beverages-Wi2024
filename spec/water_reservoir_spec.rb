61e465e6797268234843166b8537c677fbab0182
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 17:06:45 -0700

    test default initialized values in water_reservoir

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
-
+    it 'has a capacity and current water volume' do
+        reservoir = WaterReservoir.new()
+        expect(reservoir.capacity).to eq(10)
+        expect(reservoir.current_water_volume).to eq(0)
+    end
 end
