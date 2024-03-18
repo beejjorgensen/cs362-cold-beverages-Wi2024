20e29213b311c6b328e4b4f6a68cc6037579f215
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 20:08:18 -0700

    test dispense

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -13,4 +13,16 @@ describe 'A water dispenser' do
         water_dispenser.dispense(vessel)
         expect(reservoir.current_water_volume).to eq(0)
     end
+
+    it 'wont dispense more water than the reservoir has' do
+        vessel = Vessel.new('FAKE', 100)
+        water_dispenser.dispense(vessel)
+        expect(reservoir.current_water_volume).to eq(0)
+    end
+
+    it 'will fill the veseel with water' do
+        vessel = Vessel.new('FAKE', 100)
+        water_dispenser.dispense(vessel)
+        expect(vessel).to_not be_empty
+    end
 end
