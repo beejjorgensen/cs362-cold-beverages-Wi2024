1ed0cf8cdc621606a74ee832f2f9ef8b5daee4de
kameron beamer <beamerkameron@gmail.com>
2024-03-18 04:14:12 +0000

    Added water dispenser accepts reservoir test

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+    it "water_dispense accepts reservoir" do
+        reservoir = WaterReservoir.new
+        water_dispense = WaterDispenser.new(reservoir)
+        expect(reservoir).to eq(reservoir)
+    end
 
 end
