bc0bd00b0b7aef2f5bb5f34755f60a4d9b18a5ff
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 17:19:54 -0700

    test that a chiller has a capacity and a temp

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/chiller'
 
 describe 'A chiller' do
+    it ' has capacity and temperature' do
+        chiller = Chiller.new()
+        expect(chiller.capacity).to eq(100)
+        expect(chiller.temperature).to eq(70)
+    end
 
 end
