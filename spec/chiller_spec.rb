e9b7c3629665ecb847a95698156449383970d88f
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-17 17:21:43 -0700

    edit file so we can see power and contents

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../lib/chiller'
 
 describe 'A chiller' do
-    it ' has capacity and temperature' do
+    it ' has capacity, temperature, power, and contents' do
         chiller = Chiller.new()
         expect(chiller.capacity).to eq(100)
         expect(chiller.temperature).to eq(70)
+        expect(chiller.power).to eq(:off)
+        expect(chiller.contents).to eq([])
     end
+
+
 
 end
