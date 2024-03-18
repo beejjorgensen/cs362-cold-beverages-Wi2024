5511542ff26a033f2516ed510bb6e0cf0fb38c1b
kameron beamer <beamerkameron@gmail.com>
2024-03-18 04:47:50 +0000

    Added water dispenser dispense method to fill given vessel and associated test. If this has a 1 hour time limit, stop here

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,10 +1,18 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
-    it "water_dispense accepts reservoir" do
+    it "accepts reservoir" do
         reservoir = WaterReservoir.new
         water_dispense = WaterDispenser.new(reservoir)
         expect(reservoir).to eq(reservoir)
+    end
+
+    it "can fill a vessel" do
+        reservoir = WaterReservoir.new
+        water_dispense = WaterDispenser.new(reservoir)
+        vessel = Vessel.new
+        water_dispense.dispense(vessel)
+        expect(vessel).to_not be_empty
     end
 
 end
