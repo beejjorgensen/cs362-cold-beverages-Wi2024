991f9c714aef9e72355ef0402b039f5ef05b9388
kameron beamer <beamerkameron@gmail.com>
2024-03-18 05:48:56 +0000

    Added chiller tests

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -6,4 +6,25 @@ describe 'A chiller' do
         expect(chiller.capacity).to eq(100)
         expect(chiller.temperature).to eq(70)
     end
+
+    it 'is initially off' do
+        chiller = Chiller.new
+        expect(chiller.get_power).to eq(:off)
+    end
+
+    it 'can be turned on and off' do
+        chiller = Chiller.new
+        chiller.turn_on
+        expect(chiller.get_power).to eq(:on)
+        chiller.turn_off
+        expect(chiller.get_power).to eq(:off)
+    end
+
+    it 'can set temperature level' do
+        chiller = Chiller.new(temperature = 70)
+        chiller.set_level(14)
+        expect(chiller.temperature).to eq(0)
+    end
+
 end
+
